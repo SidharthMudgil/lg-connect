363d25b853d35f7a0b21f286dcc8a0365e016dd8
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-04-10 17:34:43 +0530

    inital ui created

---
+++ app/src/main/java/com/sidharth/lgconnect/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.sidharth.lgconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment: Fragment = HomeFragment()
+        val mapsFragment: Fragment = MapsFragment()
+        val controllerFragment: Fragment = ControllerFragment()
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        fragmentManager.beginTransaction()
+            .add(R.id.fragment_container, homeFragment, "home-fragment")
+            .commit()
+
+        fragmentManager.beginTransaction()
+            .add(R.id.fragment_container, controllerFragment, "controller-fragment")
+            .hide(controllerFragment)
+            .commit()
+
+        fragmentManager.beginTransaction()
+            .add(R.id.fragment_container, mapsFragment, "maps-fragment")
+            .hide(mapsFragment)
+            .commit()
+
+        var active: Fragment = homeFragment
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    fragmentManager.beginTransaction()
+                        .hide(active).show(homeFragment).commit()
+                    active = homeFragment
+                    true
+                }
+                R.id.maps -> {
+                    fragmentManager.beginTransaction()
+                        .hide(active).show(mapsFragment).commit()
+                    active = mapsFragment
+                    true
+                }
+                R.id.controller -> {
+                    fragmentManager.beginTransaction()
+                        .hide(active).show(controllerFragment).commit()
+                    active = controllerFragment
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/sidharth/lgconnect/MapsFragment.kt
@@ -0,0 +1,36 @@
+package com.sidharth.lgconnect
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+
+
+
+class MapsFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        googleMap.setMapStyle(context?.let { MapStyleOptions.loadRawResourceStyle(it, R.raw.map_style_dark) })
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(21.51, 81.23), 1f))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}

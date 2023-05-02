6e70b852f44c60789b988b2eb4b06c11f2adbaa1
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2023-05-02 12:58:52 +0530

    constant list data added to util/constants

--- app/src/main/java/com/sidharth/lgconnect/util/Constants.kt
+++ app/src/main/java/com/sidharth/lgconnect/util/Constants.kt
@@ -1,2 +1,138 @@
 package com.sidharth.lgconnect.util
 
+import com.google.android.gms.maps.model.LatLng
+import com.sidharth.lgconnect.R
+import com.sidharth.lgconnect.domain.model.Chart
+import com.sidharth.lgconnect.domain.model.Planet
+import com.sidharth.lgconnect.domain.model.Wonder
+
+object Constants {
+    val planets: List<Planet> = listOf(
+        Planet(
+            cover = R.drawable.ic_earth,
+            name = "earth",
+        ),
+        Planet(
+            cover = R.drawable.ic_moon,
+            name = "moon",
+        ),
+        Planet(
+            cover = R.drawable.ic_mars,
+            name = "mars",
+        ),
+    )
+
+    val wonders: List<Wonder> = listOf(
+        Wonder(
+            cover = R.drawable.great_wall_of_china,
+            title = "Great Wall of China",
+            subtitle = "A wall so tall, standing in pride, unity and strength fortified.",
+            description = "The Great Wall of China is one of the most iconic landmarks in the world. It is a series of fortifications that were built over centuries to protect China from invaders. The wall is so long that it can be seen from space. It is also one of the most popular tourist destinations in China.\n\nThe Great Wall was built by many different dynasties over the centuries. The first section of the wall was built in the 7th century BC. The wall was continuously expanded and rebuilt over the centuries. It reached its current size in the 16th century.\n\nThe Great Wall is made up of many different materials, including stone, brick, and earth. It is also built on a variety of terrain, including mountains, valleys, and plains. The wall is so long that it is impossible to see it all from one place.\n\nThe Great Wall is a symbol of China's strength and resilience. It is also a reminder of the country's long and rich history. The wall is a must-see for any visitor to China.",
+            address = "Bathing, Yanqing District, Beijing, China",
+            city = "Beijing",
+            state = "Beijing",
+            country = "China",
+            pin = "100101",
+            latLng = LatLng(39.904167, 116.397222),
+        ),
+        Wonder(
+            cover = R.drawable.petra_jordan,
+            title = "Petra",
+            subtitle = "Carved in sandstone, red and bright, a city lost in time, a marvel of sight.",
+            description = "Petra is an ancient city carved into the rock in Jordan. It was once the capital of the Nabataean kingdom and was a major trading center between Arabia, Egypt, and Syria. The city was founded in the 4th century BC and reached its peak in the 1st century AD. It was abandoned in the 8th century AD.\n\nPetra is home to some of the most impressive rock-cut architecture in the world, including the Treasury, the Monastery, and the Colossi of Memnon. The city is also known for its intricate water systems and its beautiful desert landscapes.\n\nPetra is a UNESCO World Heritage Site and is one of the most popular tourist destinations in Jordan. It is a must-see for any visitor to the country.",
+            address = "Petra, Ma'an Governorate 71815, Jordan",
+            city = "Petra",
+            state = "Ma'an Governorate",
+            country = "Jordan",
+            pin = "71815",
+            latLng = LatLng(30.352222, 35.862778)
+        ),
+        Wonder(
+            cover = R.drawable.christ_the_redeemer,
+            title = "Christ the Redeemer",
+            subtitle = "With arms wide open, atop a hill, a symbol of love, peace, and goodwill.",
+            description = "Christ the Redeemer is a statue of Jesus Christ in Rio de Janeiro, Brazil. It is one of the most iconic landmarks in Brazil and one of the Seven Wonders of the Modern World. The statue was built between 1922 and 1931 and stands 98 feet tall. It is located on Corcovado Mountain, which offers stunning views of Rio de Janeiro.\n\nChrist the Redeemer is a symbol of Brazil's religious and cultural heritage. It is also a popular tourist destination and is visited by millions of people each year. The statue is a reminder of Brazil's strength and resilience and is a must-see for any visitor to Rio de Janeiro.",
+            address = "Corcovado, Parque Nacional da Tijuca, Rio de Janeiro, Brazil",
+            city = "Rio de Janeiro",
+            state = "Rio de Janeiro",
+            country = "Brazil",
+            pin = "22290-000",
+            latLng = LatLng(-22.911451, -43.207528)
+        ),
+        Wonder(
+            cover = R.drawable.machu_picchu,
+            title = "Machu Picchu",
+            subtitle = "City in clouds, so high it stands, a wonder of Incans, mysterious and grand.",
+            description = "Machu Picchu is an ancient Inca city located in the Cusco Region of Peru. It was built in the 15th century AD and was abandoned in the 16th century AD. The city is located high in the Andes Mountains and is surrounded by mountains, valleys, and forests.\n\nMachu Picchu is one of the most popular tourist destinations in Peru. It is a UNESCO World Heritage Site and is one of the Seven Wonders of the Modern World. The city is a reminder of the Inca Empire's power and influence and is a must-see for any visitor to Peru.",
+            address = "Machupicchu, Urubamba 08451, Peru",
+            city = "Machupicchu",
+            state = "Urubamba",
+            country = "Peru",
+            pin = "08451",
+            latLng = LatLng(-13.168333, -72.522222)
+        ),
+        Wonder(
+            cover = R.drawable.chichen_itza,
+            title = "Chichen Itza",
+            subtitle = "A pyramid, a snake in stone, a Mayan marvel, ancient and well-known.",
+            description = "Chichen Itza is an ancient Mayan city located in the Yucatán Peninsula of Mexico. It was one of the most important cities in the Mayan world and was a major center of trade, religion, and culture. The city was founded in the 6th century AD and reached its peak in the 10th century AD. It was abandoned in the 13th century AD.\n\nChichen Itza is home to some of the most impressive Mayan ruins in the world, including the Kukulkan Pyramid, the Temple of the Warriors, and the Ball Court. The city is also known for its well-preserved water systems and its intricate stone carvings.\n\nChichen Itza is a UNESCO World Heritage Site and is one of the most popular tourist destinations in Mexico. It is a must-see for any visitor to the Yucatán Peninsula.",
+            address = "Chichén Itzá, Yucatán 77710, Mexico",
+            city = "Chichén Itzá",
+            state = "Yucatán",
+            country = "Mexico",
+            pin = "77710",
+            latLng = LatLng(20.676389, -88.568333)
+        ),
+        Wonder(
+            cover = R.drawable.roman_colosseum,
+            title = "Colosseum",
+            subtitle = "Ruins of battles, emperors' fame, an amphitheater, still etching its name.",
+            description = "The Colosseum is an oval amphitheater in the center of Rome, Italy. It was built between 70 and 80 AD by Emperor Vespasian and is the largest amphitheater of its kind ever built. The Colosseum was used for gladiatorial contests, public executions, animal hunts, and re-enactments of famous battles. It could hold up to 80,000 spectators and was one of the most popular tourist attractions in the Roman Empire.\n\nThe Colosseum is an iconic symbol of Rome and one of the most popular tourist destinations in Italy. It is a UNESCO World Heritage Site and is one of the Seven Wonders of the Modern World. The Colosseum is a reminder of the power and glory of the Roman Empire and is a must-see for any visitor to Rome.",
+            address = "Piazza del Colosseo, 00184 Roma RM, Italy",
+            city = "Rome",
+            state = "Lazio",
+            country = "Italy",
+            pin = "00184",
+            latLng = LatLng(41.890192, 12.491307)
+        ),
+        Wonder(
+            cover = R.drawable.taj_mahal,
+            title = "Taj Mahal",
+            subtitle = "A monument of love, ivory-white and grand, a tribute eternal, in gardens so grand.",
+            description = "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan (reigned 1628–1658) to house the tomb of his favourite wife Mumtaz Mahal; it also houses the tomb of Shah Jahan himself. The tomb is the centrepiece of a 17-hectare (42-acre) complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall.\n\nConstruction of the mausoleum was essentially completed in 1643 but work continued on other phases of the project for another 10 years. The Taj Mahal is widely recognized as \"the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage\". It is one of the most visited tourist destinations in India.",
+            address = "Agra, Uttar Pradesh 282003, India",
+            city = "Agra",
+            state = "Uttar Pradesh",
+            country = "India",
+            pin = "282003",
+            latLng = LatLng(27.175278, 78.039722)
+        )
+    )
+
+    val charts: List<Chart> = listOf(
+        Chart(
+            type = ChartType.BAR.type,
+            cover = R.drawable.ic_bar_chart
+        ),
+        Chart(
+            type = ChartType.LINE.type,
+            cover = R.drawable.ic_line_chart
+        ),
+        Chart(
+            type = ChartType.SCATTER.type,
+            cover = R.drawable.ic_scatter_chart
+        ),
+        Chart(
+            type = ChartType.PIE.type,
+            cover = R.drawable.ic_pie_chart
+        ),
+        Chart(
+            type = ChartType.GAUGE.type,
+            cover = R.drawable.ic_gauge_chart
+        ),
+        Chart(
+            type = ChartType.RADAR.type,
+            cover = R.drawable.ic_radar_chart
+        )
+    )
+}
